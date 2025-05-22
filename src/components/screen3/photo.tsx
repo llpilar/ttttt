d42f9236ad20e4fc768946d239a1e5284a6cb52f
llpilar <112026977+llpilar@users.noreply.github.com>
2025-05-22 23:02:00 +0200

    Update photo gallery layout

--- src/components/screen3/photo.tsx
+++ src/components/screen3/photo.tsx
@@ -1,8 +1,8 @@
-import * as THREE from 'three';
 import { Plane, useScroll, useTexture } from '@react-three/drei';
 import { useFrame } from '@react-three/fiber';
-import { useRef, useState } from 'react';
-import { Mesh } from 'three';
+import { useRef } from 'react';
+import { MathUtils, Mesh } from 'three';
+import { easeOutQuart } from './utils';
 
 type Photo = {
   src: string;
@@ -12,27 +12,35 @@ type Photo = {
 export const Photo = (props: Photo) => {
   const photo = useTexture(props.src);
   const ref = useRef<Mesh>(null);
-  const [isActive, setIsActive] = useState(props.index === 0);
   const scroll = useScroll();
+  const isOddIndex = props.index % 2 === 0;
+  const startPosition = isOddIndex ? -1.5 : 1.5;
+  const previousOffset = useRef(-1);
 
   useFrame(() => {
-    if (!ref.current) return;
+    if (!ref.current || previousOffset.current === Number(scroll.offset.toFixed(8))) {
+      return;
+    }
 
-    const scale = isActive ? 1 : 0.5;
-    ref.current.scale.x = THREE.MathUtils.lerp(ref.current.scale.x, scale, 0.1);
-    ref.current.scale.y = THREE.MathUtils.lerp(ref.current.scale.y, scale, 0.1);
+    const { x } = ref.current.position;
+    const dir = previousOffset.current > scroll.offset ? -1 : 1;
+    ref.current.position.x = isOddIndex
+      ? MathUtils.clamp(x + easeOutQuart(0.01 * dir), startPosition, 0)
+      : MathUtils.clamp(x - easeOutQuart(0.01 * dir), 0, startPosition);
+
+    previousOffset.current = Number(scroll.offset.toFixed(8));
   });
 
   return (
     <Plane
       ref={ref}
-      position-z={isActive ? 0 : -1}
-      args={[4, 5]}
+      position-x={startPosition}
+      position-y={-0.25 + Math.random() * 0.5}
+      position-z={props.index * -0.35}
+      args={[3.25, 4.5]}
       material-map={photo}
       material-transparent
-      material-colorSpace="srgb"
-      material-grayscale={1}
-      onClick={() => setIsActive(true)}
+      material-alphaTest={0.1}
     />
   );
 };
