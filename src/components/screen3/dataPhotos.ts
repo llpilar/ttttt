356aa3db419e439bf64003e1bc91ceb9f3cb4037
llpilar <112026977+llpilar@users.noreply.github.com>
2025-05-22 23:12:45 +0200

    Update image URLs to reliable sources

--- src/components/screen3/dataPhotos.ts
+++ src/components/screen3/dataPhotos.ts
@@ -1,6 +1,6 @@
 export const dataPhotos = {
-  Photo1: 'https://archleague.org/wp-content/uploads/2021/01/AMRT-ME4_Cayer-Work9_1300x961_Rumford-Historical-Society.jpg',
-  Photo2: 'https://industry.pulpandpaper-technology.com/articles/1741243267-facepaper.png',
-  Photo3: 'https://www.ramenvalves.com/wp-content/uploads/2019/07/pulp_and_paper_machine.jpg',
-  Photo4: 'https://www.tilsatec-na.com/cdn/shop/articles/Blog-Image-12-11-2018_1800x.jpg?v=1544558026'
+  Photo1: 'https://images.pexels.com/photos/2873486/pexels-photo-2873486.jpeg',
+  Photo2: 'https://images.pexels.com/photos/4483610/pexels-photo-4483610.jpeg',
+  Photo3: 'https://images.pexels.com/photos/5712159/pexels-photo-5712159.jpeg',
+  Photo4: 'https://images.pexels.com/photos/5712324/pexels-photo-5712324.jpeg'
 };
