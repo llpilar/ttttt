464277bbb61bbcc5c043c0b534908dc726aef09a
llpilar <112026977+llpilar@users.noreply.github.com>
2025-05-22 23:29:41 +0200

    Add new aerial photo to dataPhotos

--- src/components/screen3/dataPhotos.ts
+++ src/components/screen3/dataPhotos.ts
@@ -2,5 +2,6 @@ export const dataPhotos = {
   Photo1: 'https://images.pexels.com/photos/2014422/pexels-photo-2014422.jpeg',
   Photo2: 'https://images.pexels.com/photos/2014433/pexels-photo-2014433.jpeg',
   Photo3: 'https://images.pexels.com/photos/2014438/pexels-photo-2014438.jpeg',
-  Photo4: 'https://images.pexels.com/photos/2014442/pexels-photo-2014442.jpeg'
+  Photo4: 'https://images.pexels.com/photos/2014442/pexels-photo-2014442.jpeg',
+  Photo5: 'https://images.pexels.com/photos/12176420/pexels-photo-12176420.jpeg'
 };
