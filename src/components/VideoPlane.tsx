7fc241ffbe909e19ea8062b06f8f15b3b26079c9
llpilar <112026977+llpilar@users.noreply.github.com>
2025-05-22 23:31:11 +0200

    Update opening video to paper machine

--- src/components/VideoPlane.tsx
+++ src/components/VideoPlane.tsx
@@ -1,4 +1,9 @@
-import { Plane, useAspect, useScroll, useVideoTexture } from "@react-three/drei";
+import {
+  Plane,
+  useAspect,
+  useScroll,
+  useVideoTexture,
+} from "@react-three/drei";
 import { useFrame } from "@react-three/fiber";
 import { Suspense, useRef } from "react";
 import { DoubleSide, Mesh } from "three";
@@ -7,17 +12,12 @@ import { OpenerText } from "@/components/opener/openerText";
 type VideoPlane = {
   texturePath: string;
 };
-
 export const VideoPlane = ({ texturePath }: VideoPlane) => {
   const scroll = useScroll();
   const windowSize = useAspect(1920, 1080);
 
-  const videoTexture = useVideoTexture("https://player.vimeo.com/external/493786124.hd.mp4?s=e7bffc306afd2c00c5e0c5c7192a50f3a7337f8c&profile_id=175&oauth2_token_id=57447761", {
+  const videoTexture = useVideoTexture(texturePath, {
     autoplay: true,
-    loop: true,
-    muted: true,
-    crossOrigin: 'anonymous',
-    playsInline: true,
   });
 
   const ref = useRef<Mesh>(null);
