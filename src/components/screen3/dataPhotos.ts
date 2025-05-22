640b868c75f2369839132f3b1fcda95aba43c1a5
llpilar <112026977+llpilar@users.noreply.github.com>
2025-05-22 23:13:22 +0200

    Update image URLs to working Pexels links

--- src/components/screen3/dataPhotos.ts
+++ src/components/screen3/dataPhotos.ts
@@ -1,6 +1,6 @@
 export const dataPhotos = {
-  Photo1: 'https://images.pexels.com/photos/1738434/pexels-photo-1738434.jpeg',
-  Photo2: 'https://images.pexels.com/photos/1738438/pexels-photo-1738438.jpeg',
-  Photo3: 'https://images.pexels.com/photos/1738439/pexels-photo-1738439.jpeg',
-  Photo4: 'https://images.pexels.com/photos/1738442/pexels-photo-1738442.jpeg'
+  Photo1: 'https://images.pexels.com/photos/2014422/pexels-photo-2014422.jpeg',
+  Photo2: 'https://images.pexels.com/photos/2014433/pexels-photo-2014433.jpeg',
+  Photo3: 'https://images.pexels.com/photos/2014438/pexels-photo-2014438.jpeg',
+  Photo4: 'https://images.pexels.com/photos/2014442/pexels-photo-2014442.jpeg'
 };
