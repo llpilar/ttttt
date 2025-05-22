711f9f3fb4f04343d0022e3dd8a366340128c180
llpilar <112026977+llpilar@users.noreply.github.com>
2025-05-22 23:13:05 +0200

    Update image URLs to working Pexels photos

--- src/components/screen3/dataPhotos.ts
+++ src/components/screen3/dataPhotos.ts
@@ -1,6 +1,6 @@
 export const dataPhotos = {
-  Photo1: 'https://images.pexels.com/photos/2873486/pexels-photo-2873486.jpeg',
-  Photo2: 'https://images.pexels.com/photos/4483610/pexels-photo-4483610.jpeg',
-  Photo3: 'https://images.pexels.com/photos/5712159/pexels-photo-5712159.jpeg',
-  Photo4: 'https://images.pexels.com/photos/5712324/pexels-photo-5712324.jpeg'
+  Photo1: 'https://images.pexels.com/photos/1738434/pexels-photo-1738434.jpeg',
+  Photo2: 'https://images.pexels.com/photos/1738438/pexels-photo-1738438.jpeg',
+  Photo3: 'https://images.pexels.com/photos/1738439/pexels-photo-1738439.jpeg',
+  Photo4: 'https://images.pexels.com/photos/1738442/pexels-photo-1738442.jpeg'
 };
